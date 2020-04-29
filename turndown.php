3224ab4de7bf274f19598d58a41f98eda5219214
MohannadRayani <50885252+MohannadRayani@users.noreply.github.com>
2020-04-29 13:21:09 -0500

    Add files via upload

---
+++ turndown.php
@@ -0,0 +1,4 @@
+<?php shell_exec("sudo python3 /home/pi/yturndown.py");
+header("Location: http://192.168.1.5");
+exit();
+?>
---
+++ turnup.php
@@ -0,0 +1,4 @@
+<?php shell_exec("sudo python3 /home/pi/yturnup.py");
+header("Location: http://192.168.1.5");
+exit();
+?>
